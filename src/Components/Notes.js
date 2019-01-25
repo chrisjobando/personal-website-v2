a40b932e8839e70d33bdf700940ae3fbc7a1704e
Christopher Obando <chrisjobando@gmail.com>
2019-01-25 19:59:32 +0100

    Work on adding form signup, must convert to JSX code

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -22,6 +22,39 @@ class Notes extends Component {
       <header style={ myStyles }>
         <div className='rect'>
           <h1>{ this.props.title }</h1>
+          <div className="notes-signup-box">
+
+            <div id="mc_embed_signup">
+              <form action="https://github.us7.list-manage.com/subscribe/post?u=93526193f4277ff64a96284e0&amp;id=29966d00de"
+                method="post" id="mc-embedded-subscribe-form"
+                name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+
+                  <div className="form-display">
+                    <div className="mc-field-group">
+                    	<label for="mce-FNAME">First Name </label>
+                    	<input type="text" value="" name="FNAME" class="" id="mce-FNAME" />
+                    </div>
+
+                    <div className="mc-field-group">
+                    	<label for="mce-EMAIL">Email Address </label>
+                    	<input type="email" value="" name="EMAIL" class="required email"
+                        id="mce-EMAIL" />
+                    </div>
+
+                    <div className="clear"><input type="submit" value="Subscribe" name="subscribe"
+                      id="mc-embedded-subscribe" class="button" /></div>
+                  </div>
+
+                  <div id="mce-responses" class="clear">
+                  	<div className="response" id="mce-error-response"></div>
+                  	<div className="response" id="mce-success-response"></div>
+                  </div>
+                </div>
+            </form>
+            </div>
+
+          </div>
           <div className="col-1">
             <p>Chris Notes are short weekly emails that include a topic I was interested in that week,
                recommendations for cool media I enjoy, and progress on my projects!</p>
