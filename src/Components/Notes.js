9b05d0546269ecbf6e4774432604251baafc0cfd
Christopher Obando <chrisjobando@gmail.com>
2019-01-28 10:45:20 +0100

    Fixed links for Notes

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -63,9 +63,26 @@ class Notes extends Component {
             <div>
               <p>Here is an archive to previous Notes that I have released:</p>
               <ul>
-                <li>First Note</li>
-                <li>Second Note</li>
-                <li>Third Note</li>
+                <li>
+                  <a href="https://us7.campaign-archive.com/?u=93526193f4277ff64a96284e0&id=ec6712d2c6"
+                    target="_blank">First Note
+                  </a>
+                </li>
+                <li>
+                  <a href="https://mailchi.mp/7a12828d7734/k6q8ettyhq-2260421"
+                    target="_blank">Second Note
+                  </a>
+                </li>
+                <li>
+                  <a href="https://mailchi.mp/28d9b0a98edd/k6q8ettyhq-2265173"
+                    target="_blank">Third Note
+                  </a>
+                </li>
+                <li>
+                  <a href="https://mailchi.mp/9cea98cd793d/k6q8ettyhq-2278305"
+                    target="_blank">Fourth Note
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
