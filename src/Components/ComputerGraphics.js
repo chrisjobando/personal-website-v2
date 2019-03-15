aaadf9f17054d136a20c0d9676d57d2668255dee
Christopher Obando <chrisjobando@gmail.com>
2019-03-15 01:03:25 +0100

    More responsive images on cg page

--- src/Components/ComputerGraphics.js
+++ src/Components/ComputerGraphics.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 // Section Background
 import Background from '../img/header-bg.jpg';
 
+//Styling
+import '../SASS/cg-page.sass';
+
 // Pictures
 import Assignment1 from '../img/ComputerGraphics/Assignment1.png';
 import Assignment11 from '../img/ComputerGraphics/Assignment1_Obando.png';
@@ -18,17 +21,17 @@ const myStyles = {
 class ComputerGraphics extends Component {
   render() {
     return (
-      <div className="gba" style={ myStyles }>
+      <div className="cg" style={ myStyles }>
         <div className="rect">
             <title>computer graphics</title>
             <h1>This is My <span>Computer Graphics</span> Work.</h1>
 
             <h2>Assignment 1: Pov Ray Scene Recreation</h2>
-            <img style={{width: '60%'}} src={Assignment1} alt=' '/><br/>
-            <img style={{width: '60%'}} src={Assignment11} alt=' '/><br/>
+            <img src={Assignment1} alt=' '/><br/>
+            <img src={Assignment11} alt=' '/><br/>
             <br/>
             <h2>Assignment 2: Terragen Scene</h2>
-            <img style={{width: '80%'}} src={Assignment2} alt=' '/>
+            <img src={Assignment2} alt=' '/>
         </div>
       </div>
     );
